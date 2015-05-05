6a786bdf189b0fce05e49cbbb18702e66a15796c
zapstart <zapstart@gmail.com>
2015-05-04 22:06:20 -0700

    Second version of Lab10

--- Lab10_TrafficLight/TableTrafficLight.c
+++ Lab10_TrafficLight/TableTrafficLight.c
@@ -22,20 +22,77 @@
 #include "tm4c123gh6pm.h"
 
 // ***** 2. Global Declarations Section *****
+#define init    0
+#define goN     1
+#define waitN   2
+#define goE     3
+#define waitE   4
+#define walk    5
+#define rushW   6
+#define dontW   7
+
+typedef const struct state {
+    U32 output;
+    U32 delay;
+    U32 Next[8];
+}traffic_light, *ptraffic_light;
+
+traffic_light TL_FSM[x] = {
+    {x, x, {}},
+    {x, x, {}},
+    {x, x, {}},
+    {x, x, {}},
+    {x, x, {}},
+    {x, x, {}},
+    {x, x, {}},
+    {x, x, {}},
+    {x, x, {}},
+    {x, x, {}},
+}
 
 // FUNCTION PROTOTYPES: Each subroutine defined
 void DisableInterrupts(void); // Disable interrupts
 void EnableInterrupts(void);  // Enable interrupts
 
 // ***** 3. Subroutines Section *****
-
+void GPIO_INIT(void);
+   
 int main(void){ 
-  TExaS_Init(SW_PIN_PE210, LED_PIN_PB543210); // activate grader and set system clock to 80 MHz
+    TExaS_Init(SW_PIN_PE210, LED_PIN_PB543210); 
  
-  
-  EnableInterrupts();
-  while(1){
+    
+    EnableInterrupts();
+    while(1){
      
-  }
+    }
+}
+
+void GPIO_INIT(void) {
+    volatile U64 delay;
+    
+    SYSCTL_RCGC2_R |= 0x12;     
+    delay = SYSCTL_RCGC2_R;           
+    
+    // init port B 
+    GPIO_PORTB_AMSEL_R &= ~0xFF;        
+    GPIO_PORTB_PCTL_R &= ~0xFF;   
+    GPIO_PORTB_DIR_R |= 0x3F;          
+    GPIO_PORTB_AFSEL_R &= 0xFF;        
+    GPIO_PORTB_DEN_R |= 0x3F;          
+    
+    // init port E
+    GPIO_PORTE_AMSEL_R &= ~0xFF;        
+    GPIO_PORTE_PCTL_R &= ~0xFF;   
+    GPIO_PORTE_DIR_R &= ~0x07;          
+    GPIO_PORTE_AFSEL_R &= ~0xFF;        
+    GPIO_PORTE_DEN_R |= 0x07;          
+
+    // init port F 
+    GPIO_PORTB_AMSEL_R &= ~0xFF;        
+    GPIO_PORTB_PCTL_R &= ~0xFF;   
+    GPIO_PORTB_DIR_R |= 0x0A;          
+    GPIO_PORTB_AFSEL_R &= ~0xFF;        
+    GPIO_PORTB_PUR_R |= 0x0A;          
+    GPIO_PORTB_DEN_R |= 0x0A;          
 }
 
