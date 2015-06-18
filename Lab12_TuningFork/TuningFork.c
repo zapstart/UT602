704397bd7a7c2073a4f096cb4ef3c5ac46fd4609
zapstart <zapstart@gmail.com>
2015-06-18 12:02:46 -0700

    Modify GPIO and NVIC init; Add SysTic_Handle

--- Lab12_TuningFork/TuningFork.c
+++ Lab12_TuningFork/TuningFork.c
@@ -44,22 +44,55 @@ void DisableInterrupts(void); // Disable interrupts
 void EnableInterrupts(void);  // Enable interrupts
 void WaitForInterrupt(void);  // low power mode
 
+// init systick 
+void SysTick_Init(U32 period) {
+    NVIC_ST_CTRL_R    = 0;
+    NVIC_ST_RELOAD_R  = period - 1;
+    NVIC_ST_CURRENT_R = 0;
+    NVIC_SYS_PRI3_R   = (NVIC_SYS_PRI3_R & 0x00FFFFFF) | 0x40000000;
+    NVIC_ST_CTRL_R    = 0x7;
+}
+
+// init GPIO ports
+void GPIO_Init(void) {
+    SYSCTL_RCGC2_R      |= 0x00000001;      
+    GPIO_PORTA_AMSEL_R  &= 0x00;        
+    GPIO_PORTA_DIR_R    &= ~0x08;         
+    GPIO_PORTA_DIR_R    |= 0x04;          
+    GPIO_PORTA_AFSEL_R  &= 0x00;        
+    GPIO_PORTA_PUR_R    |= 0x10;          
+    GPIO_PORTA_DEN_R    |= 0x0B;          
+    GPIO_PORTA_DATA_R   &= ~0x08;
+}
+
 // input from PA3, output from PA2, SysTick interrupts
 void Sound_Init(void){ 
-
+    SysTick_Init(90900);
+    GPIO_Init();
 }
 
 // called at 880 Hz
 void SysTick_Handler(void){
-
+    U16 en = 0;
+    if (GPIO_PORTA_DATA_R & 0x08 && en == 0) {
+        en = 1; 
+    }   
+    if (GPIO_PORTA_DATA_R & 0x08 && en == 1) {
+        en = 0; 
+    }   
+    if (en) {     
+        GPIO_PORTA_DATA_R ^= 0x04;         
+    }
 }
 
 int main(void){// activate grader and set system clock to 80 MHz
   TExaS_Init(SW_PIN_PA3, HEADPHONE_PIN_PA2,ScopeOn); 
   Sound_Init();         
   EnableInterrupts();   // enable after all initialization are done
+     
   while(1){
     // main program is free to perform other tasks
     // do not use WaitForInterrupt() here, it may cause the TExaS to crash
+    Sound_Init();
   }
 }
